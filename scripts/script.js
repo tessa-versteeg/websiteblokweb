40d5c84beda3aa690cfba0ee16338abf9468c88c
tessa-versteeg <70432910+tessa-versteeg@users.noreply.github.com>
2020-10-05 11:05:14 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+var hamburgerButton = document.querySelector('nav button');
+
+var subMenu = document.querySelector('nav ul');
+
+
+function toggleMenu() {
+    subMenu.classList.toggle('is-open');
+}
+
+
+hamburgerButton.addEventListener('click', toggleMenu);
